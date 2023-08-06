207dad0f846ccbea3a23fc0547b5e23f1464816b
KekmaTime <136650032+KekmaTime@users.noreply.github.com>
2023-08-06 06:29:40 +0530

    fixed json output to normal

--- Easy/Stocks.js
+++ Easy/Stocks.js
@@ -1,5 +1,5 @@
 var apiKey = 'Y6VAMZXHF4GKMSSM';
-var symbol = 'GOOGL';
+var symbol = 'GOOGL'; //you can use any other company code as
 
 async function getStock() {
     var Url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=GOOGL&apikey=' + apiKey;
@@ -10,9 +10,13 @@ async function getStock() {
 
         console.log(data);
 
-        var Daily = data['Time Series (Daily)'];
-
-
+        const timeSeries = data['Time Series (Daily)'];
+        for (const date in timeSeries) {
+            if (timeSeries.hasOwnProperty(date)) {
+              const dailyData = timeSeries[date];
+              console.log('Date:', date,'High:', dailyData['2. high']);
+            }
+        }
     } catch(error) {
         console.log('Error fetching data: ' + error);
     }
