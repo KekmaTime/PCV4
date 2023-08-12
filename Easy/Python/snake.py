a68f82ae647832d38bd680dae9ce6a801ea43fc2
KekmaTime <136650032+KekmaTime@users.noreply.github.com>
2023-08-12 20:43:14 +0530

    snake basic boilerplate

---
+++ Easy/Python/snake.py
@@ -0,0 +1,38 @@
+from tkinter import *
+import random
+
+GAME_WIDTH = 600
+GAME_HEIGHT = 600
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+
+def check_collison():
+    pass
+
+def game_over():
+    pass
+
+window = Tk()
+window.title("Snake LMAO LOL")
+window.resizable(False, False)
+
+window.mainloop()
