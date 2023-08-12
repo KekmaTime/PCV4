b1e28d0dde1470f5bb41ede2541822d4581d5c00
KekmaTime <136650032+KekmaTime@users.noreply.github.com>
2023-08-13 00:16:16 +0530

    snake def completed

--- Easy/Python/snake.py
+++ Easy/Python/snake.py
@@ -12,7 +12,19 @@ BACKGROUND_COLOR = "#000000"
 
 
 class Snake:
-    pass
+    
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = SNAKE_COLOR, tag="snek")
+            self.squares.append(square)
 
 class Food:
 
@@ -25,7 +37,7 @@ class Food:
         self.coordinates = [x, y]
 
         canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = FOOD_COLOR, tag = 'food')
-        
+
 
 def next_turn():
     pass
