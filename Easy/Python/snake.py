5e40c225823b0c0e003015ed742d94fe7053dd4d
KekmaTime <136650032+KekmaTime@users.noreply.github.com>
2023-08-12 22:03:07 +0530

    food class completed

--- Easy/Python/snake.py
+++ Easy/Python/snake.py
@@ -15,8 +15,17 @@ class Snake:
     pass
 
 class Food:
-    pass
 
+    def __init__(self):
+
+        x = random.randint(0, (GAME_WIDTH/SPACE_SIZE)-1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT/SPACE_SIZE)-1) * SPACE_SIZE
+
+
+        self.coordinates = [x, y]
+
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = FOOD_COLOR, tag = 'food')
+        
 
 def next_turn():
     pass
@@ -43,5 +52,23 @@ label.pack()
 
 canvas = Canvas(window, bg=BACKGROUND_COLOR, width=GAME_WIDTH, height=GAME_HEIGHT)
 canvas.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
+
+
+
 
 window.mainloop()
